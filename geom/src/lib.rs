0eafe3950c45c0e5bc3912d12c16a4601b9fd012
Danilo Guanabara <danguafer@gmail.com>
2021-02-10 15:22:27 -0300

    Update lib.rs

--- geom/src/lib.rs
+++ geom/src/lib.rs
@@ -5,8 +5,9 @@ pub struct Rectangle {
     height: i32
 }
 
-#[ligen(UE4Blueprint)]
+#[ligen(UE4Blueprint(BlueprintCallable, BlueprintType))]
 impl Rectangle {
+    #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
     pub fn new(width: i32, height: i32) -> Rectangle {
         Rectangle {
             width,
@@ -14,42 +15,25 @@ impl Rectangle {
         }
     }
 
-    // pub fn get_vec(&self) -> Vec<i32> { Vec::new() }
+    #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
+    pub fn get_width() -> i32 { self.width }
 
-    pub fn get_width(&self) -> i32 { self.width }
-    pub fn get_height(&self) -> i32 { self.height }
+    #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
+    pub fn get_height() -> i32 { self.height }
 
-    pub fn print_type() { println!("Rectangle"); }
+    #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
+    pub fn print_type(&self) {
+        println!("Rectangle(width: {}, height: {})", self.width, self.height);
+    }
 
+    #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
     pub fn add(&self, rect: &Rectangle) -> Rectangle {
-        Rectangle::new(self.get_width() + rect.get_width(), self.get_height() + rect.get_height())
+        Rectangle {
+            width: self.width + rect.width,
+            height: self.height + rect.height
+        }
     }
 }
-
-
-// #[ligen(UE4Blueprint(BlueprintCallable, BlueprintType))]
-// impl Rectangle {
-//     #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
-//     pub fn new(width: i32, height: i32) -> Rectangle {
-//         Rectangle {
-//             width,
-//             height
-//         }
-//     }
-//
-//     #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
-//     pub fn digits() -> i32 { 987654321 }
-//
-//     #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
-//     pub fn print(&self) {
-//         println!("Rectangle(width: {}, height: {})", self.width, self.height);
-//     }
-//
-//     #[ligen(UE4Blueprint(BlueprintCallable, Category = "Rectangle"))]
-//     pub fn add_widths(&self, rect: &Rectangle) -> i32 {
-//         self.width + rect.width
-//     }
-// }
 
 impl Drop for Rectangle {
     fn drop(&mut self) {
